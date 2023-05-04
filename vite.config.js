7e46c7f05a2e77cf1ce216ddf0f2300f5358110a
Martin老師 <tpemartin@gmail.com>
2023-05-04 17:01:02 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myApp01/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
